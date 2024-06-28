0538a9dcf62f4f90416259ff0a1921a36c4eb93f
Michel Escalante Álvarez <michel.escalante@sequra.com>
2024-06-28 12:45:00 +0200

    feat: Add Tools module with sayhi and saybye methods
    The code changes introduce a new module called `Tools` with two methods: `sayhi` and `saybye`. These methods print out "Hello" and "Good Bye" respectively. The commit message reflects the addition of this module and its functionality.

---
+++ 17/tools.rb
@@ -0,0 +1,9 @@
+module Tools
+  def sayhi
+    puts "Hello"
+  end
+
+  def saybye
+    puts "Good Bye"
+  end
+end
---
+++ 17_modules.rb
@@ -0,0 +1,4 @@
+require_relative "17/tools.rb"
+include Tools
+Tools.sayhi
+Tools.saybye
